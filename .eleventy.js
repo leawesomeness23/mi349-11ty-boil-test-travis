3f89d308535a1ad3926f2eec372ca435c8f37687
leawesomeness23 <travisdr@msu.edu>
2025-12-03 23:33:20 -0500

    hopefully this is works
    w

--- .eleventy.js
+++ .eleventy.js
@@ -3,4 +3,18 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+
+
+  eleventyConfig.addshortcode("itemcard", function(name, image, description, link) {
+    return `
+      <div class="item-card">
+        <img src="${image}" alt="${name}" class="item-card-image"/>
+        <h3>${name}</h3>
+        <p>${description}</p>
+        <a href="${link}" class="btn">Learn More</a>
+      </div>
+
+
+    `;
+  });
 };
