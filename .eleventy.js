67981f651512e0d75d0843da6ef080a7b002ad4b
leawesomeness23 <travisdr@msu.edu>
2025-12-04 01:09:20 -0500

    Might take break
    Working on css styling right now

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,5 @@
-
 // The export statement makes these settings available to other files in 11ty
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
-
-
-  eleventyConfig.addshortcode("itemcard", function(name, image, description, link) {
-    return `
-      <div class="item-card">
-        <img src="${image}" alt="${name}" class="item-card-image"/>
-        <h3>${name}</h3>
-        <p>${description}</p>
-        <a href="${link}" class="btn">Learn More</a>
-      </div>
-
-
-    `;
-  });
 };
